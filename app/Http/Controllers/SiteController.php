bab9b899c395c8be3aabbc5e83723446f7745e28
MoisesBumbae <moises-alberto@hotmail.comm>
2021-03-08 02:02:57 +0100

    3-commit pagina principal

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Abaut;
+
+
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
+    private $about; 
+
+    public function __construct(Abaut $about)
+    {   
+        
+        $this->about = $about; 
+    }
+
     public function index() 
 
     {
-        return view('site.home.index');
+        $title = 'Clínica Grande Muralha da China'; 
+
+        
+        $dadosabout = $this->about
+                    ->where('status', 'A')
+                    ->get();
+
+        return view('site.home.index', compact('title', 'dadosabout') ); 
+        
     }
+ 
 }
